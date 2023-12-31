9ed5c59ac22f5f72a519e1c0101874a827e41fc1
jams10 <jamin3710@gmail.com>
2023-12-31 18:59:43 +0900

    001. Created a project.

---
+++ TheIsland/Source/TheIsland/TheIsland.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheIsland.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheIsland, "TheIsland" );
