12f30507641a2c7b379b2b59604a5d588f336aa3
jams10 <jamin3710@gmail.com>
2024-01-07 21:58:23 +0900

    018. Added CameraMode & CameraModeStack class.

---
+++ TheIsland/Source/TheIsland/Camera/TICameraMode.cpp
@@ -0,0 +1,14 @@
+﻿#include "TICameraMode.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(TICameraMode)
+
+UTICameraMode::UTICameraMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
+
+UTICameraModeStack::UTICameraModeStack(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
---
+++ TheIsland/Source/TheIsland/Camera/TICameraMode.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "TICameraMode.generated.h"
+
+/*
+*	UTICameraMode
+*	개별 카메라 시점을 구현한 클래스. ex) 비조준, 조준시 시점 모두 개별 카메라 모드로 만들어 사용함.
+*/
+UCLASS(Abstract, NotBlueprintable)
+class UTICameraMode : public UObject
+{
+	GENERATED_BODY()
+public:
+	UTICameraMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
+
+/*
+*	UTICameraModeStack
+*	카메라 모드간에 부드럽게 전환 되도록 카메라 모드들을 관리하는 클래스.
+*/
+UCLASS()
+class UTICameraModeStack : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UTICameraModeStack(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	/*
+		member variables
+	*/
+	// 생성된 camera mode를 관리.
+	UPROPERTY()
+	TArray<TObjectPtr<UTICameraMode>> CameraModeInstances;
+
+	// Camera Matrix Blend 업데이트 진행 큐
+	UPROPERTY()
+	TArray<TObjectPtr<UTICameraMode>> CameraModeStack;
+};
