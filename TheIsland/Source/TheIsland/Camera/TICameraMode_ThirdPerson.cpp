6e7fb5e1e3e46c7037d85b0f1552928fa13cc99a
jams10 <jamin3710@gmail.com>
2024-01-07 23:35:11 +0900

    020. Added a camera mode for ThirdPerson.

---
+++ TheIsland/Source/TheIsland/Camera/TICameraMode_ThirdPerson.cpp
@@ -0,0 +1,8 @@
+﻿#include "TICameraMode_ThirdPerson.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(TICameraMode_ThirdPerson)
+
+UTICameraMode_ThirdPerson::UTICameraMode_ThirdPerson(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+
+}
---
+++ TheIsland/Source/TheIsland/Camera/TICameraMode_ThirdPerson.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "TICameraMode.h"
+#include "TICameraMode_ThirdPerson.generated.h"
+
+/*
+*	UTICameraMode_ThirdPerson
+*	3인칭 카메라 모드.
+*/
+UCLASS(Abstract, Blueprintable)
+class UTICameraMode_ThirdPerson : public UTICameraMode
+{
+	GENERATED_BODY()
+public:
+	UTICameraMode_ThirdPerson(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
