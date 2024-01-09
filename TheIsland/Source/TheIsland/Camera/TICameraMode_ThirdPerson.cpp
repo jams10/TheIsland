485ba0a29923c79fe14e9d5897849a52b9c70381
jams10 <jamin3710@gmail.com>
2024-01-09 22:57:26 +0900

    023. Added ThirdPerson camera mode.

--- TheIsland/Source/TheIsland/Camera/TICameraMode_ThirdPerson.cpp
+++ TheIsland/Source/TheIsland/Camera/TICameraMode_ThirdPerson.cpp
@@ -1,8 +1,32 @@
 ﻿#include "TICameraMode_ThirdPerson.h"
+#include "Curves/CurveVector.h"
 #include UE_INLINE_GENERATED_CPP_BY_NAME(TICameraMode_ThirdPerson)
 
-UTICameraMode_ThirdPerson::UTICameraMode_ThirdPerson(const FObjectInitializer& ObjectInitializer)
-	:Super(ObjectInitializer)
+UTICameraMode_ThirdPerson::UTICameraMode_ThirdPerson()
 {
+	TargetOffsetCurve = nullptr;
+}
 
+void UTICameraMode_ThirdPerson::UpdateView(float DeltaTime)
+{
+	// Base class인 CameraMode의 UpdateView와 동일함.
+	// 이 코드에 추가적으로 우리가 각 특정 모드에 따라 구현해야 할 사항들을 추가해주면 됨.
+	FVector PivotLocation = GetPivotLocation();
+	FRotator PivotRotation = GetPivotRotation();
+
+	PivotRotation.Pitch = FMath::ClampAngle(PivotRotation.Pitch, ViewPitchMin, ViewPitchMax);
+
+	View.Location = PivotLocation;
+	View.Rotation = PivotRotation;
+	View.ControlRotation = View.Rotation;
+	View.FieldOfView = FieldOfView;
+
+	// TargetOffsetCurve가 override 되어 있따면, Curve에 있는 값을 가져와서 적용함.
+	// - Camera 관점에서 Character의 어느 부분에 Target으로 할지 결정하는 것으로 이해하면 됨.
+	if (TargetOffsetCurve)
+	{
+		// PivotRotation의 Pitch 값을 입력으로 해서 Curve 값을 받아옴.
+		const FVector TargetOffset = TargetOffsetCurve->GetVectorValue(PivotRotation.Pitch);
+		View.Location = PivotLocation + PivotRotation.RotateVector(TargetOffset);
+	}
 }
--- TheIsland/Source/TheIsland/Camera/TICameraMode_ThirdPerson.h
+++ TheIsland/Source/TheIsland/Camera/TICameraMode_ThirdPerson.h
@@ -2,6 +2,8 @@
 #include "TICameraMode.h"
 #include "TICameraMode_ThirdPerson.generated.h"
 
+class UCurveVector;
+
 /*
 *	UTICameraMode_ThirdPerson
 *	3인칭 카메라 모드.
@@ -11,5 +13,16 @@ class UTICameraMode_ThirdPerson : public UTICameraMode
 {
 	GENERATED_BODY()
 public:
-	UTICameraMode_ThirdPerson(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	UTICameraMode_ThirdPerson();
+
+	/*
+		UTICameraMode's Interface
+	*/
+	virtual void UpdateView(float DeltaTime) override;
+
+	/*
+		member variables
+	*/
+	UPROPERTY(EditDefaultsOnly, Category = "Third Person")
+	TObjectPtr<const UCurveVector> TargetOffsetCurve;
 };
