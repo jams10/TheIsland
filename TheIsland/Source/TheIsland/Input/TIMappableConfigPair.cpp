9f084f3550129c3e9317a09f840d993bdca36fcc
jams10 <jamin3710@gmail.com>
2024-01-11 16:29:59 +0900

    027. Added MappableConfigPair structure.

---
+++ TheIsland/Source/TheIsland/Input/TIMappableConfigPair.cpp
@@ -0,0 +1,4 @@
+﻿#include "TIMappableConfigPair.h"
+#include "PlayerMappableInputConfig.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(TIMappableConfigPair)
---
+++ TheIsland/Source/TheIsland/Input/TIMappableConfigPair.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "TIMappableConfigPair.generated.h"
+
+class UPlayerMappableInputConfig;
+
+USTRUCT()
+struct FTIMappableConfigPair
+{
+	GENERATED_BODY()
+
+public:
+	FTIMappableConfigPair() = default;
+
+	UPROPERTY(EditAnywhere)
+	TSoftObjectPtr<UPlayerMappableInputConfig> Config;
+
+	UPROPERTY(EditAnywhere)
+	bool bShouldActivateAutomatically = true;
+};
