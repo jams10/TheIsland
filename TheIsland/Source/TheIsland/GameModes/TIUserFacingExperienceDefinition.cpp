b832a8086bbed19c028425b0acc6f7a6890f9b55
jams10 <jamin3710@gmail.com>
2024-01-12 18:19:25 +0900

    034. Added CreateHostingRequest() function to create UCommonSession_HostSessionRequest instance for transitioning a map.

--- TheIsland/Source/TheIsland/GameModes/TIUserFacingExperienceDefinition.cpp
+++ TheIsland/Source/TheIsland/GameModes/TIUserFacingExperienceDefinition.cpp
@@ -1,3 +1,18 @@
 ﻿#include "TIUserFacingExperienceDefinition.h"
+#include "CommonUser/Public/CommonSessionSubsystem.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(TIUserFacingExperienceDefinition)
+
+UCommonSession_HostSessionRequest* UTIUserFacingExperienceDefinition::CreateHostingRequest() const
+{
+	const FString ExperienceName = ExperienceID.PrimaryAssetName.ToString();
+
+	// UCommonSession_HostSessionRequest 클래스는 UObject를 상속 받은 클래스이기 때문에, 인스턴스는 알아서 GC에 의해 관리됨.
+	// 이 인스턴스의 경우 현재 프레임을 벗어나면 GC에 의해 제거되므로, 만약 추가적인 프레임에서 사용할 경우, 
+	// dangling 포인터 문제를 방지하기 위해 객체 수명 주기를 따로 관리해주기 위해 멤버 변수로 선언하는 등 관리가 필요함.
+	UCommonSession_HostSessionRequest* Result = NewObject<UCommonSession_HostSessionRequest>();
+	Result->MapID = MapID;
+	Result->ExtraArgs.Add(TEXT("Experience"), ExperienceName);
+
+	return Result;
+}
--- TheIsland/Source/TheIsland/GameModes/TIUserFacingExperienceDefinition.h
+++ TheIsland/Source/TheIsland/GameModes/TIUserFacingExperienceDefinition.h
@@ -4,6 +4,8 @@
 #include "Engine/DataAsset.h" // UPrimaryDataAsset 상속을 위함.
 #include "TIUserFacingExperienceDefinition.generated.h"
 
+class UCommonSession_HostSessionRequest;
+
 /*
 * TIUserFacingExperienceDefinition
 * Experience를 적용하기 위한 진입점. Experience가 적용될 Map, 적용할 Experience의 정보가 담긴 ExperienceDefinition을 담고 있음.
@@ -21,4 +23,8 @@ public:
 	// 불러올 게임 플레이 experience, 게임 모드의 ID.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Experience, meta = (AllowedTypes = "TIExperienceDefinition"))
 	FPrimaryAssetId ExperienceID;
+
+	// Map 로딩 및 Experience 전환을 위해, MapID와 ExperienceID를 활용하여, HostSessionRequest를 생성하는 함수.
+	UFUNCTION(BlueprintCallable)
+	UCommonSession_HostSessionRequest* CreateHostingRequest() const;
 };
